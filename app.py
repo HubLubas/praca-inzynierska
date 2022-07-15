20b4e446ef1a75da5566466243ba1849eac3c309
HubLubas <hub.lubas@gmail.com>
2022-07-15 18:47:15 +0200

    sma

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, url_for, redirect, request, jsonify
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField
 from random import randint, seed
-import time
-import yfinance as yf
-import datetime
-from datetime import date, timedelta
 from technical import get_prices
 
 app = Flask(__name__)
@@ -19,17 +15,13 @@ def index():
     '''
     return render_template('index.html')
 
-@app.route('/technical/', methods=['GET'])
-def technical():
-    '''Classification endpoint'''
-    
-    #response = yf.download("AZN.L", start=datetime.datetime(2010, 1, 1), 
-    #                                  end=datetime.datetime(2019, 12, 31), group_by='tickers')
-    #response.headers.add('Access-Control-Allow-Origin', '*')
-    
-    response  = get_prices()
-    
-    return response
+#@app.route('/technical/', methods=['GET'])
+#def technical():
+#    '''Classification endpoint'''
+#        
+#    response  = get_prices()
+#    
+#   return response
 
 
 
---
+++ technical_my.py
@@ -0,0 +1,35 @@
+import yfinance as yf
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+from matplotlib.dates import date2num, DateFormatter, WeekdayLocator,\
+    DayLocator, MONDAY
+import seaborn as sns
+
+import mplfinance as mpf
+from mplfinance.original_flavor import candlestick_ohlc
+
+from scipy import stats
+from scipy.stats import zscore
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import coint
+
+import datetime
+from datetime import date, timedelta
+
+import warnings
+warnings.filterwarnings('ignore')
+
+ftse100_stocks = yf.download("AZN.L", start=datetime.datetime(2010, 1, 1), 
+                                     end=datetime.datetime(2019, 12, 31), group_by='tickers')
+print(ftse100_stocks["Adj Close"].rolling(window=20).mean())
+
+#Simple Moving Average
+def sma(win_wide, start_date, end_date):
+    sma = ftse100_stocks["Adj Close"].loc[start_date:end_date].rolling(window=win_wide).mean()
+    return sma;
+
+print(sma(20, '2019-01-01','2019-12-31'))
