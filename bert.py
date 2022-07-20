260118c79a287d8940ba3904c72488e3ebe5db3d
HubLubas <hub.lubas@gmail.com>
2022-07-20 20:08:43 +0200

    bert init

---
+++ bert.py
@@ -0,0 +1,118 @@
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import nltk
+import time
+import datetime
+import random
+import warnings
+warnings.filterwarnings('ignore')
+
+# Tokenize to add stopwords
+from nltk import word_tokenize
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+
+# Data loading utility
+from torch.utils.data import Dataset, TensorDataset, DataLoader, RandomSampler, SequentialSampler, random_split
+
+# Models
+from torch import nn
+from transformers import BertModel, BertTokenizer, BertForSequenceClassification, AdamW, get_linear_schedule_with_warmup
+from transformers import logging
+logging.set_verbosity_error()
+
+# Training
+from torch import optim
+
+# Evaluation
+from sklearn.metrics import accuracy_score, matthews_corrcoef
+
+article_sentiments = pd.read_pickle('azn_prices_labels_news_20210107.pkl')
+model = torch.load('bert_model')
+test_data = pd.read_pickle('test_data')
+#print(test_data.head())
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True) 
+# Create sentence and label lists
+articles = test_data.filtered_articles_split.values
+labels = test_data.Label.values
+
+# Tokenise all of the sentences and map the tokens to their word IDs.
+input_ids = []
+attention_masks = []
+
+# For every sentence...
+for article in articles:
+    # `encode_plus` will:
+    #   (1) Tokenize the sentence.
+    #   (2) Prepend the `[CLS]` token to the start.
+    #   (3) Append the `[SEP]` token to the end.
+    #   (4) Map tokens to their IDs.
+    #   (5) Pad or truncate the sentence to `max_length`
+    #   (6) Create attention masks for [PAD] tokens.
+    encoded_dict = tokenizer.encode_plus(
+                        article,                   # Sentence to encode.
+                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                        max_length = 200,           # Pad and truncate all sentences.  
+                        return_token_type_ids=False,
+                        pad_to_max_length = True,
+                        return_attention_mask = True,   # Construct attention masks.
+                        return_tensors = 'pt',     # Return pytorch tensors.
+                   )
+    
+    # Add the encoded sentence to the list.    
+    input_ids.append(encoded_dict['input_ids'])
+    
+    # And its attention mask (simply differentiates padding from non-padding).
+    attention_masks.append(encoded_dict['attention_mask'])
+
+# Convert the lists into tensors.
+input_ids = torch.cat(input_ids, dim=0)
+attention_masks = torch.cat(attention_masks, dim=0)
+labels = torch.tensor(labels)
+
+# Set the batch size.  
+batch_size = 16  
+
+# Create the DataLoader.
+prediction_data = TensorDataset(input_ids, attention_masks, labels)
+prediction_sampler = SequentialSampler(prediction_data)
+prediction_data_loader = DataLoader(prediction_data, sampler=prediction_sampler, batch_size=batch_size)
+
+# Put model in evaluation mode
+model.eval()
+
+# Tracking variables 
+predictions, true_labels = [], []
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Predict 
+for batch in prediction_data_loader:
+  # Add batch to GPU
+  batch = tuple(t.to(device) for t in batch)
+  
+  # Unpack the inputs from our dataloader
+  b_input_ids, b_input_mask, b_labels = batch
+  
+  # Telling the model not to compute or store gradients, saving memory and 
+  # speeding up prediction
+  with torch.no_grad():
+      # Forward pass, calculate logit predictions
+      outputs = model(b_input_ids, token_type_ids=None, 
+                      attention_mask=b_input_mask)
+
+  logits = outputs[0]
+
+  # Move logits and labels to CPU
+  logits = logits.detach().cpu().numpy()
+  label_ids = b_labels.to('cpu').numpy()
+  
+  # Store predictions and true labels
+  predictions.append(logits)
+  true_labels.append(label_ids)
+
+print('    DONE.')
+print('Positive samples: %d of %d (%.2f%%)' % (test_data.Label.sum(), len(test_data.Label), (test_data.Label.sum() / len(test_data.Label) * 100.0)))
